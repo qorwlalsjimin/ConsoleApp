ae6d061b1a92a6134d983da2aeb71fd70b3b106b
Jimin Baek <qorwlalsjimin@naver.com>
2023-04-17 12:09:11 +0900

    SetCursorPosition을 변경시켜서 지렁이 움직이게 하기

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.Write("메서드 호출");
+/*            Console.Write("메서드 호출");
             Thread.Sleep(1000); //1초 //Thread 문제로 정확하지 않다
             Console.SetCursorPosition(5, 5); //화면 표시는 절댓값
             Console.WriteLine("메서드 호출2");
@@ -18,7 +18,26 @@ namespace ConsoleApp
             Console.SetCursorPosition(2, 2);
             Console.WriteLine("메서드 호출3");
 
-            //Console.SetCursorPosition(0, 0); //위에 출력 내용을 덮을 수 있다
+            //Console.SetCursorPosition(0, 0); //위에 출력 내용을 덮을 수 있다*/
+
+            int x = 1;
+            while(x < 50)
+            {
+                Console.Clear();
+                Console.SetCursorPosition(x, 5);
+
+                if(x%3 == 0)
+                {
+                    Console.WriteLine("__@");
+                }else if(x%3 == 1)
+                {
+                    Console.WriteLine("_^@");
+                }else { 
+                    Console.WriteLine("^_@"); 
+                }
+                Thread.Sleep(1000);
+                x++;
+            }
         }
     }
 }
